6f835f3f4e2c1c5f48fadaa838fc49dba5589b4b
qmzrjf <e.babay@flyaps.com>
2025-03-28 21:01:09 +0200

    Third commit

---
+++ lesson2/lesson.py
@@ -0,0 +1,70 @@
+# import math
+print("start!")
+# VAR = 10
+
+
+
+# my_var = 10
+#
+# MY_VAR = 20
+#
+# birth_date = 1997
+#
+# print(birth_date)
+# # print(type(birth_date))
+# print(id(birth_date))
+#
+# birth_date = birth_date
+# print(id(birth_date))
+#
+#
+# print("________________")
+
+# birth_date = "March 25"
+# print(birth_date)
+# print(type(birth_date))
+# print(id(birth_date))
+
+# snake_case, cameCase, PascalCase, kebab-case
+
+# lst = [1, 2, 3]
+# print(lst)
+# print(type(lst))
+# print(id(lst))
+#
+# print("_____________")
+# lst.append(4)
+# print(lst)
+# print(type(lst))
+# print(id(lst))
+
+
+# i1 = 6
+# i2 = 3
+#
+# f1 = 10.1
+# f2 = 5.0
+#
+# print(i1 + i2)
+# print(f1 - f2)
+# print(i1 * i2)
+# print(i1 / i2)
+# print(i1 + f2)
+#
+# print(i1 % i2)
+# print(i1 // i2)
+#
+# div, mod = divmod(11.1, 2.2)
+#
+# print(div, mod)
+#
+# print(4 ** 0.5)
+#
+#
+# import math
+# import random
+# import os
+# import sys
+#
+# print(math.sqrt(9))
+#
